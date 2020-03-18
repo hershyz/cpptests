b54e1f1f4f5b95f05375791c8f234c4f0a9d3c61
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2020-03-17 20:57:31 -0400

    Add files via upload

---
+++ headers with public variables/Properties.cpp
@@ -0,0 +1,11 @@
+#include "Properties.h"
+#include <iostream>
+using namespace std;
+
+//Constructor:
+Properties::Properties() {
+
+}
+
+string currentDir;
+string version = "v0.1";
---
+++ headers with public variables/Properties.h
@@ -0,0 +1,13 @@
+#ifndef PROPERTIES_H
+#define PROPERTIES_H
+#include <iostream>
+using namespace std;
+
+class Properties {
+    public:
+        Properties();
+        string version = "v0.1";
+        string currentDir;
+};
+
+#endif
